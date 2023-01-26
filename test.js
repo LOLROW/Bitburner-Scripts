9129b8160be1f0be943b03ab491f63bbc45db66d
LOLROW <32370118+LOLROW@users.noreply.github.com>
2023-01-25 20:31:30 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    var target = ns.args[0];
-    var minMoney = ns.getServerMaxMoney(target) * 0.75;
-    var maxSecurity = ns.getServerSecurityLevel(target) + 5;
- 
-    while (true) {
-        if (ns.getServerMoneyAvailable(target) < minMoney || ns.getServerSecurityLevel(target) > maxSecurity) {
-            await ns.grow(target); await ns.weaken(target);
-        }
-        else {
-            await ns.hack(target);
-        }
-    }
+	var target = ns.args[0];
+	var minimumMoney = ns.getServerMaxMoney(target) * 0.5;
+	var maximumSecurityLevel = ns.getServerSecurityLevel(target) + 5;
+
+	ns.nuke(target);
+
+	while (true) {
+		if (ns.getServerMoneyAvailable(target) < minimumMoney || ns.getServerMinSecurityLevel(target) > maximumSecurityLevel) {
+			await ns.grow(target); await ns.weaken(target);
+		}
+		await ns.hack(target)
+	}
 }
